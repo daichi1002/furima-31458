ec091727df91c2c9cc9afd98d6bf3ef5746a4fee
daichi1002 <72916511+daichi1002@users.noreply.github.com>
2020-11-05 10:59:06 +0900

    Userモデルの単体テストコード

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,14 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  
-  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
-  validates_format_of :password, with: PASSWORD_REGEX, message: "Include both letters and numbers"
+
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'Include both letters and numbers'
   validates :nickname,           presence: true
-  validates :first_name,         presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "Full-width characters."}
-  validates :last_name,          presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "Full-width characters."}
-  validates :first_name_reading, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: "Full-width katakana characters."}
-  validates :last_name_reading,  presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: "Full-width katakana characters."}
+  validates :first_name,         presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: 'Full-width characters.' }
+  validates :last_name,          presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: 'Full-width characters.' }
+  validates :first_name_reading, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: 'Full-width katakana characters.' }
+  validates :last_name_reading,  presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: 'Full-width katakana characters.' }
   validates :birth_day,          presence: true
 
   has_many :items
